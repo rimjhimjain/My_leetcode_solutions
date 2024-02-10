b0fcc0be571650abb096e52ca345d98e5dee4835
Rimjhim Jain <98699402+rimjhimjain@users.noreply.github.com>
2024-02-10 13:21:28 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (31.05%) - LeetSync

--- 682-baseball-game/baseball-game.cpp
+++ 682-baseball-game/baseball-game.cpp
@@ -3,18 +3,13 @@ public:
     int calPoints(vector<string>& operations) {
 
         vector<int> ans;
-        int n;
 
         for(int i = 0;i < operations.size();i++){
-            // cout << "g";
             if(operations[i] == "D"){
-                // if (!ans.empty()) {
                     int insert = ans.back() * 2;
                     ans.push_back(insert);
-                // }
             }else if (operations[i] == "+") {
                     int sum = ans[ans.size() - 1] + ans[ans.size() - 2];
-                    cout << " sum = " << sum << endl;
                     ans.push_back(sum);
             }else if(operations[i] == "C"){
                 ans.pop_back();
@@ -23,10 +18,8 @@ public:
             }
             
         }
-        cout << ans.size();
         int sum =0;
         for(int i = 0;i < ans.size();i++){
-            cout << ans[i]<< " ";
             sum += ans[i];
         }
         return sum;
