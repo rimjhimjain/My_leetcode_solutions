e1b3dc8f51a244abcc1e6ca4c903aa4d4869485f
Rimjhim Jain <98699402+rimjhimjain@users.noreply.github.com>
2024-02-16 14:58:13 +0530

    Time: 51 ms (46.27%) | Memory: 50.9 MB (31.35%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -6,8 +6,6 @@ public:
 
         for(int i = 0;i < n;i++){
            if(i > reach) return false;
-            cout << "n = "<< n << endl;
-            cout << "reach = "<< reach << endl;
             reach = max(reach,i+nums[i]);
         }
 
