987e30e6222c7a26c58671d10c0c05ef4b5c13f2
Rimjhim Jain <98699402+rimjhimjain@users.noreply.github.com>
2024-02-21 23:31:46 +0530

    Time: 16 ms (92.41%) | Memory: 28.4 MB (36.57%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
+        ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
         int n = nums.size();
         vector<int> ans(n);
         for(int i = 0;i < n;i++){
