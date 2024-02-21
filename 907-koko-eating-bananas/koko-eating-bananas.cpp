873a834a7c053629e9295861109c9ceee63a0add
Rimjhim Jain <98699402+rimjhimjain@users.noreply.github.com>
2024-02-21 12:27:49 +0530

    Time: 33 ms (64.08%) | Memory: 22.1 MB (10.20%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -2,30 +2,45 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         sort(piles.begin(),piles.end());
-        int n = piles.size()-1;
 
-        if(piles.size() == h){
-            return piles[n];
-        }
+        int n = piles.size();
+
+        if(n == h) return piles[n-1];
 
         int left = 1;
-        int right = piles[n];
-        int hourneed = 0;
-        int count = 0;
+        int right = piles[n-1];
 
-        while(left < right){
-            hourneed = left + (right - left)/2;
-                count = 0;
-            for(int i = 0;i < piles.size();i++){
-                count += (piles[i] + hourneed -1)/hourneed;
+        long hourneed = 0;
+        long count = 0;
+
+        while(left <= right){
+            hourneed = left + (right-left)/2;
+
+            count = 0;
+            for(int i = 0;i < n;i++ ){
+                if(hourneed > piles[i]){
+                    count += 1;
+                }else{
+                    int d = piles[i]/hourneed;
+                    count += d;
+                    if(piles[i]%hourneed != 0){
+                        count+=1;
+                    }
+                }
             }
-
+                // cout << hourneed << " " << count << endl;
             if(count > h){
                 left = hourneed+1;
             }else{
-                right = hourneed;
+                right = hourneed-1;
             }
+            // cout << "left = "<<left <<endl;
+            // cout << "right = "<<right <<endl;
+            
         }
         return left;
+
     }
 };
+
+//4,11,20,23,30
