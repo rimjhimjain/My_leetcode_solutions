0f62dedff6a774d0c851445f124c124abceb61ea
Rimjhim Jain <98699402+rimjhimjain@users.noreply.github.com>
2024-02-22 00:06:30 +0530

    Time: 12 ms (34.18%) | Memory: 10.3 MB (10.17%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+        unordered_map<char,int> smap;
+        unordered_map<char,int> tmap;
+
+        for(int i=0;i<s.length();i++){
+            smap[s[i]]++;
+            tmap[t[i]]++;
+        }
+
+        for(int i=0;i<smap.size();i++){
+            if(smap[i] != tmap[i]) return false;
+        }
+
+        return true;
+    }
+};
