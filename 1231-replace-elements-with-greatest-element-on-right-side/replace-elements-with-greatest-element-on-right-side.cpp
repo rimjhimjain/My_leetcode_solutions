918c3e798eb88db15914ddd27b4683c000891fb5
Rimjhim Jain <98699402+rimjhimjain@users.noreply.github.com>
2024-02-27 20:12:11 +0530

    Time: 73 ms (79.51%) | Memory: 71 MB (23.27%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        int n = arr.size();
+        int n = arr.size()-1;
         int maxi = *max_element(arr.begin(),arr.end());
 
-        for(int i = 0;i < arr.size()-1;i++){
+        for(int i = 0;i < n;i++){
             if(arr[i] == maxi){
                 int n = i+1;
                 maxi = *max_element(arr.begin()+n,arr.end());
             }
             arr[i] = maxi;
-            cout << arr[i] << " ";
         }
         arr.pop_back();
         arr.push_back(-1);
