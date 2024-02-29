184a6ab7155778108630fb0c98e7ce5ccb776933
Rimjhim Jain <98699402+rimjhimjain@users.noreply.github.com>
2024-02-29 14:22:15 +0530

    Time: 66 ms (5.07%) | Memory: 13.4 MB (20.79%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    class Pair{
+        public:
+        int num;
+        string roman;
+    };
+    string intToRoman(int num) {
+
+        vector<Pair> pairs = {
+            {1000,"M"},
+            {900,"CM"},
+            {500,"D"},
+            {400,"CD"},
+            {100,"C"},
+            {90,"XC"},
+            {50,"L"},
+            {40,"XL"},
+            {10,"X"},
+            {9,"IX"},
+            {5,"V"},
+            {4,"IV"},
+            {1,"I"}
+        };
+
+        string roman;
+        int i = 0;
+
+        while (num > 0) {
+            if (num >= pairs[i].num) {
+                roman += pairs[i].roman;
+                num -= pairs[i].num;
+            } else {
+                i++;
+            }
+            cout << num << endl;
+        }
+
+        return roman;
+
+    }
+};
