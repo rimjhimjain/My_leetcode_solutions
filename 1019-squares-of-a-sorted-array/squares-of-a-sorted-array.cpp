88d5ee8c07ff0b5d547f2b041562923f9ca8f321
Rimjhim Jain <98699402+rimjhimjain@users.noreply.github.com>
2024-03-02 23:11:21 +0530

    Time: 29 ms (33.34%) | Memory: 30.1 MB (12.76%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -7,22 +7,21 @@ public:
         for(int i = 0;i < n;i++){
             nums[i]*=nums[i];
         }
-        
-        // 16,1,0,9,100
-        int left = 0;
-        int right = n-1;
-        int index = n-1;
+        sort(nums.begin(),nums.end());
+        // int left = 0;
+        // int right = n-1;
+        // int index = n-1;
 
-        while(left <= right){
-            if(nums[left] >= nums[right]){
-                ans[index] = nums[left];
-                left++;
-            }else{
-                ans[index] = nums[right];
-                right--;
-            }
-            index--;
-        }
-        return ans;
+        // while(left <= right){
+        //     if(nums[left] >= nums[right]){
+        //         ans[index] = nums[left];
+        //         left++;
+        //     }else{
+        //         ans[index] = nums[right];
+        //         right--;
+        //     }
+        //     index--;
+        // }
+        return nums;
     }
 };
