2db8261ca9ddeee1cdd64976e3e9f215b8cafeb3
Rimjhim Jain <98699402+rimjhimjain@users.noreply.github.com>
2024-03-05 16:54:00 +0530

    Time: 14 ms (41.83%) | Memory: 22.7 MB (39.65%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& bed, int n) {
+        int size = bed.size();
+        if(n==0) return true;
+        
+        for(int i = 0;i < size;i++){
+            if( (i == 0 || bed[i-1] == 0) && (bed[i] == 0) && (i == size-1 || bed[i+1] == 0 )){
+                bed[i] = 1;
+                n--;
+            }
+            //     if(i == 0 && bed[i] == 0 && bed[i+1] == 0){
+            //         bed[i] = 1;
+            //         n--;
+            //     }
+            //     else if(i == size-1 && bed[i] == 0 && bed[i-1] == 0){
+            //         bed[i] = 1;
+            //         n--; 
+            //     }
+            // else if(i != 0 && i != size-1 && bed[i-1] == 0 && bed[i] == 0 && bed[i+1] == 0){
+            //     bed[i] = 1;
+            //     n--;
+            // }
+            if(n == 0) return true;
+        }
+        // for(int i = 0;i < size;i++){
+        //     cout << bed[i] << " ";
+        // }
+
+        return false;
+    }
+};
+
+
