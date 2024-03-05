fce2455c14dfde76aa530e0a66ee27ad1fd5880e
Rimjhim Jain <98699402+rimjhimjain@users.noreply.github.com>
2024-03-05 16:55:07 +0530

    Time: 7 ms (94.01%) | Memory: 22.9 MB (9.34%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -9,23 +9,9 @@ public:
                 bed[i] = 1;
                 n--;
             }
-            //     if(i == 0 && bed[i] == 0 && bed[i+1] == 0){
-            //         bed[i] = 1;
-            //         n--;
-            //     }
-            //     else if(i == size-1 && bed[i] == 0 && bed[i-1] == 0){
-            //         bed[i] = 1;
-            //         n--; 
-            //     }
-            // else if(i != 0 && i != size-1 && bed[i-1] == 0 && bed[i] == 0 && bed[i+1] == 0){
-            //     bed[i] = 1;
-            //     n--;
-            // }
+            
             if(n == 0) return true;
         }
-        // for(int i = 0;i < size;i++){
-        //     cout << bed[i] << " ";
-        // }
 
         return false;
     }
