db525215bdc114ffd1fbd6d1dd7bf37742f296cd
Rimjhim Jain <98699402+rimjhimjain@users.noreply.github.com>
2024-03-06 15:13:50 +0530

    Time: 24 ms (24.73%) | Memory: 35.3 MB (10.13%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        ios_base::sync_with_stdio( 0 );
-    std::cin.tie( 0 );
         int n = nums.size();
-        int mid = 0;
-        int leftsum = 0,rightsum = 0;
-        while(mid < n){
-            leftsum = 0;
-            rightsum = 0;
-            for(int i = 0;i < mid;i++){
-                leftsum += nums[i];
-            }
-            for(int i = mid+1;i < n;i++){
-                rightsum += nums[i];
-            }
-            if(leftsum == rightsum){
-                return mid;
-            }
-            mid++;
+        vector<int> left(n,0);
+        vector<int> right(n,0);
+        for(int i  = 1;i < n;i++){
+            left[i] = left[i-1]+nums[i-1];
         }
-        
+        for(int i = n-2;i > -1;i--){
+            right[i] = right[i+1] + nums[i+1];
+        }
+        int i =0;
+        for( i  = 0;i < n;i++){
+            if(right[i] == left[i]){
+                return i;
+            }
+        }
         return -1;
     }
 };
