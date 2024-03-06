8d60d0474a094ad5bb0e2bbad3114d1deb7acc9e
Rimjhim Jain <98699402+rimjhimjain@users.noreply.github.com>
2024-03-06 15:07:08 +0530

    Time: 753 ms (12.24%) | Memory: 33.8 MB (74.05%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        
+        ios_base::sync_with_stdio( 0 );
+    std::cin.tie( 0 );
         int n = nums.size();
         int mid = 0;
         int leftsum = 0,rightsum = 0;
