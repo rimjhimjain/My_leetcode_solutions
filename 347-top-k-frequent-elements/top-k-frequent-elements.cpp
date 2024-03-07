7d77fefaf1b415207d007afc2662f9ba2a7fdf46
Rimjhim Jain <98699402+rimjhimjain@users.noreply.github.com>
2024-03-07 21:53:37 +0530

    Time: 15 ms (33.92%) | Memory: 17.3 MB (28.86%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        unordered_map<int, int> nmap;
+        vector<int> answer;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nmap.find(nums[i]) != nmap.end()) {
+                nmap[nums[i]] += 1;
+            } else {
+                nmap[nums[i]] = 1;
+            }
+        }
+        for(const auto j : nmap){
+            cout << j.first << " " << j.second << endl;
+        }
+
+        vector<pair<int, int>> pairs(nmap.begin(), nmap.end());
+
+        int maxi = 0;
+        vector<int> ans;
+        int p = pairs.size();
+        for (const auto j : pairs) {
+            if(p == k){
+                ans.push_back(j.first);
+            }
+            maxi = max(j.second, maxi);
+        }
+        if(ans.size() == k) return ans;
+
+        int i = 0;
+        while(k > -1 && i < p){
+            if(pairs[i].second == maxi){
+                ans.push_back(pairs[i].first);
+                k--;
+            }
+            if(k != 0 && i == p-1){
+                i = 0;
+                maxi--;
+            }
+            i++;
+        }
+
+        return ans;
+    }
+};
