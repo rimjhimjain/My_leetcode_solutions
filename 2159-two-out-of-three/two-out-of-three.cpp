ccddaa2e90dd85e5bf6fb4d7b3e95fecf416bfdc
Rimjhim Jain <98699402+rimjhimjain@users.noreply.github.com>
2024-03-15 15:39:15 +0530

    Time: 18 ms (34.50%) | Memory: 31.9 MB (36.93%) - LeetSync

--- 2159-two-out-of-three/two-out-of-three.cpp
+++ 2159-two-out-of-three/two-out-of-three.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     vector<int> twoOutOfThree(vector<int>& a, vector<int>& b, vector<int>& c) {
-        unordered_set<int> mp1(a.begin(),a.end());
-        unordered_set<int> mp2(b.begin(),b.end());
-        unordered_set<int> mp3(c.begin(),c.end());
+        unordered_set<int> mp1(a.begin(), a.end());
+        unordered_set<int> mp2(b.begin(), b.end());
+        unordered_set<int> mp3(c.begin(), c.end());
         unordered_set<int> res;
-        
-        for(int i = 0;i <a.size();i++){
-            if(mp2.find(a[i]) != mp2.end() || mp3.find(a[i]) != mp3.end()){
+
+        for (int i = 0; i < a.size(); i++) {
+            if (mp2.find(a[i]) != mp2.end() || mp3.find(a[i]) != mp3.end()) {
                 res.insert(a[i]);
             }
         }
 
-        for(int i = 0;i <b.size();i++){
-            if(mp1.find(b[i]) != mp1.end() || mp3.find(b[i]) != mp3.end()){
+        for (int i = 0; i < b.size(); i++) {
+            if (mp1.find(b[i]) != mp1.end() || mp3.find(b[i]) != mp3.end()) {
                 res.insert(b[i]);
             }
         }
 
-        for(int i = 0;i <c.size();i++){
-            if(mp2.find(c[i]) != mp2.end() || mp1.find(c[i]) != mp1.end()){
+        for (int i = 0; i < c.size(); i++) {
+            if (mp2.find(c[i]) != mp2.end() || mp1.find(c[i]) != mp1.end()) {
                 res.insert(c[i]);
             }
         }
         vector<int> ans;
-        for(auto x:res)ans.push_back(x);
-        
+        for (auto x : res)
+            ans.push_back(x);
+
         return ans;
     }
 };
