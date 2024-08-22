3b5682f2b8dd5407310b7332e077dd7416b09254
Rimjhim Jain <98699402+rimjhimjain@users.noreply.github.com>
2024-08-22 12:33:58 +0530

    Time: 56 ms (94.57%) | Memory: 86.1 MB (50.09%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -9,23 +9,27 @@ public:
 
         int bill5 = 0;
         int bill10 = 0;
-        for(int i = 0;i < bills.size();i++){
-            if(bills[i] == 5){
-                bill5++;
-            }else if(bills[i] == 10){
-                if(bill5 == 0) return false;
-                bill5--;
-                bill10++;
-            }else{
-                if(bill5 == 0) return false;
-                if(bill10 != 0){
-                    bill10--;
+        for(int b : bills){
+            switch(b){
+                case 5:
+                    bill5++;
+                    break;
+                case 10:
+                    if(bill5 == 0) return false;
                     bill5--;
-                }else{
-                    bill5 -= 3;
+                    bill10++;
+                    break;
+                case 20:
+                    if(bill10 != 0){
+                        bill10--;
+                        bill5--;
+                    } else{
+                        bill5 -= 3;
+                    }
                     if(bill5 < 0) return false;
-                }
+                    break;
             }
+
         }
         return true;
     }
