fb126cb871cfc78b5343f74653536c926cf3c053
Rimjhim Jain <98699402+rimjhimjain@users.noreply.github.com>
2024-10-10 00:03:48 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (32.94%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+
+        int count = 0;
+        for (char i : s) {
+            if (i == '(') {
+                st.push(i);
+            } else {
+                if (st.empty()) {
+                    count++;
+                } else {
+                    st.pop();
+                }
+            }
+        }
+        return count + st.size();
+    }
+};
